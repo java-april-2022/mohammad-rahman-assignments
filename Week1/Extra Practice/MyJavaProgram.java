f0f30f9e5bb85a962538ab66664863b6d5f7ab5c
morahm <72052463+morahm@users.noreply.github.com>
2022-04-05 00:38:12 -0400

    Practice Exercise - My Own Java Program

---
+++ Week1/Extra Practice/MyJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Mohammad Rahman");
+        System.out.println("I am 43 years old");
+        System.out.println("My hometown is Falls Church, VA");
+    }
+}
