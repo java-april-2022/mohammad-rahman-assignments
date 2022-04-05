135dbd0dc3de0df597c241b6180a4a0ad305e947
morahm <72052463+morahm@users.noreply.github.com>
2022-04-05 16:56:14 -0400

    Practice - Java Cafe

---
+++ Week1/Extra Practice/CafeJava.java
@@ -0,0 +1,80 @@
+import java.text.DecimalFormat;
+
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+
+        // Camel Case
+        double mochaPrice = 3.5;
+        double dripCoffeePrice = 4.25;
+        double lattePrice = 6.45;
+        double cappucinoPrice = 5.75;
+        
+        // Pascal Case
+        double DripCoffeePrice = 0.25;
+        double LattePrice = 0.45;
+        double CappucinoPrice = 0.75;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+        
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+        
+        
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+    	// ** Your customer interaction print statements will go here ** //
+        
+        // Cindhuri ordered a coffee. Print to the console the correct status message, based on her order status.
+        if (isReadyOrder1) {
+            System.out.println(customer1 + readyMessage);
+        }
+        else {
+            System.out.println(customer1 + pendingMessage);
+        }
+        
+        // Noah ordered a cappucino. Use an if statement to check the status of his order and print the correct status message. If it is ready, also print the message to display the total. Note: Outcomes may be different depending on what you assigned as the status.
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappucinoPrice);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
+
+        // Sam just ordered 2 lattes, print the message to display their total. Next, use an if statement to print the appropriate order status message. Change the isReady flag value from true to false or vice versa in order to test your control logic (if-statement).
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.println(generalGreeting + customer2);
+        System.out.println(displayTotalMessage + df.format((lattePrice * 2)));
+
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else {
+            System.out.println(customer2 + pendingMessage);
+        }
+
+        // Jimmy just realized he was charged for a coffee but had ordered a latte. Print the total message with the new calculated total (what he owes) to make up the difference.
+
+        System.out.println(generalGreeting + customer3);
+        System.out.println(displayTotalMessage + df.format(lattePrice - dripCoffeePrice));
+    }
+}
