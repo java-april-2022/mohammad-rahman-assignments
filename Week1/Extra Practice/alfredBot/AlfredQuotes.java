f9774b7c075d35d0b55197db658e105415e725a1
morahm <72052463+morahm@users.noreply.github.com>
2022-04-06 22:17:55 -0400

    alfredBot Basic Version

--- Week1/Extra Practice/alfredBot/AlfredQuotes.java
+++ Week1/Extra Practice/alfredBot/AlfredQuotes.java
@@ -1,4 +1,3 @@
-
 import java.util.Date;
 
 public class AlfredQuotes {
@@ -10,17 +9,31 @@ public class AlfredQuotes {
     
     public String guestGreeting(String name) {
         // YOUR CODE HERE
-        return "place holder for guest greeting return string";
+        // return "place holder for guest greeting return string";
+        // "Hello, Beth Kane. Lovely to see you."
+
+        return String.format("Hello, %s. Lovely to see you.", name);
     }
+
     
     public String dateAnnouncement() {
         // YOUR CODE HERE
-        return "place holder for date announcement return string";
+        // "It is currently Wed Aug 11 16:34:59 PDT 2022."
+        // return "place holder for date announcement return string";
+        Date date = new Date();
+        return String.format("It is currently %s", date);
     }
     
     public String respondBeforeAlexis(String conversation) {
         // YOUR CODE HERE
-        return "for snarky response return string";
+        // return "for snarky response return string";
+        if (conversation.indexOf("Alexis") != -1) {
+            return "Right away, sir. She certainly isn't sophisticated enough for that.";
+        }
+        else if (conversation.indexOf("Alfred") != -1) {
+            return "At your service. As you wish, naturally.";
+        }
+        else return "Right. And with that I shall retire.";
     }
     
 	// NINJA BONUS
