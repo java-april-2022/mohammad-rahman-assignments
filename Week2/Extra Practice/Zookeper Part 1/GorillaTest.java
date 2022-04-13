a1a2c4d721703a12446acff25a81a644215accbe
morahm <72052463+morahm@users.noreply.github.com>
2022-04-13 16:45:54 -0400

    Practice - Zoo Keeper Part 1

---
+++ Week2/Extra Practice/Zookeper Part 1/GorillaTest.java
@@ -0,0 +1,24 @@
+public class GorillaTest {
+    
+    public static void main(String[] args) {
+
+        Gorilla gorilla1 = new Gorilla(100);
+
+        gorilla1.throwSomething();
+        gorilla1.throwSomething();
+        gorilla1.throwSomething();
+        
+        gorilla1.displayEnergy();
+
+        gorilla1.eatBanana();
+        gorilla1.eatBanana();
+        
+        gorilla1.displayEnergy();
+
+        gorilla1.climb();
+
+        gorilla1.displayEnergy();
+        
+
+    }
+}
