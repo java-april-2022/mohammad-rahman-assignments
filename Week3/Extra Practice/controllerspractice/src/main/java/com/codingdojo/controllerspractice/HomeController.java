cc8ee3dca8ab4ba8ae53af4bff9b43528bf52a40
morahm <72052463+morahm@users.noreply.github.com>
2022-04-17 16:53:09 -0400

    Practice - Restcontroller - handler class

--- Week3/Extra Practice/controllerspractice/src/main/java/com/codingdojo/controllerspractice/HomeController.java
+++ Week3/Extra Practice/controllerspractice/src/main/java/com/codingdojo/controllerspractice/HomeController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello") //handler class (class level)
 public class HomeController {
-	@RequestMapping("/") //handler method (method level)
+	@RequestMapping("") //handler method (method level)
 	public String hello() {
 		return "Hello World!";
+	}
+	@RequestMapping("/world")
+	public String world() {
+		return "Class level annotations are cool too!";
 	}
 }
