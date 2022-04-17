32756487846abb334b1244e746c39a964190db0e
morahm <72052463+morahm@users.noreply.github.com>
2022-04-17 16:49:09 -0400

    Practice - RestController - handler method

--- Week3/Extra Practice/controllerspractice/src/main/java/com/codingdojo/controllerspractice/HomeController.java
+++ Week3/Extra Practice/controllerspractice/src/main/java/com/codingdojo/controllerspractice/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	@RequestMapping("/")
+	@RequestMapping("/") //handler method (method level)
 	public String hello() {
 		return "Hello World!";
 	}
