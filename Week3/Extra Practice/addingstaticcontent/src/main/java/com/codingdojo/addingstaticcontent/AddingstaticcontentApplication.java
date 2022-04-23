69631eca6206ba8f4a4e3ca4901427d3d3e03928
morahm <72052463+morahm@users.noreply.github.com>
2022-04-23 16:36:26 -0400

    Practice - Adding Static Content - initial commit

---
+++ Week3/Extra Practice/addingstaticcontent/src/main/java/com/codingdojo/addingstaticcontent/AddingstaticcontentApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.addingstaticcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddingstaticcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddingstaticcontentApplication.class, args);
+	}
+
+}
---
+++ Week3/Extra Practice/addingstaticcontent/src/test/java/com/codingdojo/addingstaticcontent/AddingstaticcontentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.addingstaticcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddingstaticcontentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
