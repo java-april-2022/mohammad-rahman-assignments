14599a3ab6571bbdc0c952357be5dae4f23e2ad5
morahm <72052463+morahm@users.noreply.github.com>
2022-05-20 03:48:37 -0400

    update

--- Week4/Extra Practice/mvc/src/main/java/com/codingdojo/mvc/controllers/BookController.java
+++ Week4/Extra Practice/mvc/src/main/java/com/codingdojo/mvc/controllers/BookController.java
@@ -12,12 +12,17 @@ package com.codingdojo.mvc.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.codingdojo.mvc.models.Book;
@@ -63,6 +68,22 @@ public class BookController {
 		
 		return "redirect:/books";
 	}
-
+    
+    @GetMapping("/books/{id}/edit")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.findBook(id);
+        model.addAttribute("book", book);
+        return "edit.jsp";
+    }
+    
+    @PutMapping("/books/{id}")
+    public String update(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "edit.jsp";
+        } else {
+            bookService.updateBook(book);
+            return "redirect:/books";
+        }
+    }
 
 }
